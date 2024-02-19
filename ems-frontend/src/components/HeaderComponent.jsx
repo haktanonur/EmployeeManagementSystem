2c19ba1e9e5ccf5875e2cd05c77bc3e0c23842c5
Onur Haktan <onurhaktan243@gmail.com>
2024-02-20 01:49:06 +0300

    add header and footer to react app

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className='container-fluid navbar-brand' href="#">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
