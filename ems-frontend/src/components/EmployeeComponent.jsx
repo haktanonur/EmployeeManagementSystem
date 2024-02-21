e6b51ec60c90f8f3776030d582db4652152e4fa9
Onur Haktan <onurhaktan243@gmail.com>
2024-02-21 22:48:16 +0300

    connect react to getEmployee and updateEmployee endpoints

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { createEmployee } from '../services/EmployeeServices'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeServices'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const EmployeeComponent = () => {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+
+    const {id} = useParams();
 
     const [errors, setErrors] = useState({
       firstName:'',
@@ -16,20 +18,43 @@ const EmployeeComponent = () => {
 
     const navigator = useNavigate();
 
-    function saveEmployee(e){
+    useEffect(() => {
+      
+      if(id){
+        getEmployee(id).then(response=> {
+            setFirstName(response.data.firstName);
+            setLastName(response.data.lastName);
+            setEmail(response.data.email);
+        }).catch(error => {
+            console.error(error);
+        })
+      }
+
+    }, [id])
+
+    function saveOrUpdateEmployee(e){
       e.preventDefault();
 
       if(validateForm()){
+
         const employee = {firstName, lastName, email}
         console.log(employee)
-  
-        createEmployee(employee).then((response) => {
-        console.log(response.data);
-        navigator('/employees')
-        })
-      }
-      else{
 
+        if(id){
+          updateEmployee(id, employee).then((response) => {
+              console.log(response.data);
+              navigator('/employees');
+          }).catch(error => {
+              console.error(error);
+          })
+        } else {
+          createEmployee(employee).then((response) => {
+              console.log(response.data);
+              navigator('/employees')
+          }).catch(error => {
+              console.error(error);
+          })
+        }
       }
     }
 
@@ -63,16 +88,34 @@ const EmployeeComponent = () => {
       return valid;
     }
 
+    function pageTitle(){
+        if(id){
+          return <h2 className='text-center'>Update Employee</h2>
+        } else {
+          return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
+    function buttonName(){
+      if(id){
+        return <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Update</button>
+      } else {
+        return <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Add</button>
+      }
+  }
+
   return (
     <div className='container'>
       <br /> <br />
       <div className='row'>
         <div className='card col-md-6 offset-md-3 offset-md-3'>
-          <h2 className='text-center'>Add Employee</h2>
+          {
+            pageTitle()
+          }
           <div className='card-body'>
             <form>
               <div className='form-group mb-2'>
-                <label className='form-label'>First Name:</label>
+                <label className='form-label'>First Name</label>
                 <input 
                   type="text"
                   placeholder='Enter Employee First Name'
@@ -86,7 +129,7 @@ const EmployeeComponent = () => {
               </div>
 
               <div className='form-group mb-2'>
-                <label className='form-label'>Last Name:</label>
+                <label className='form-label'>Last Name</label>
                 <input 
                   type="text"
                   placeholder='Enter Employee Last Name'
@@ -100,7 +143,7 @@ const EmployeeComponent = () => {
               </div>
 
               <div className='form-group mb-2'>
-                <label className='form-label'>Email:</label>
+                <label className='form-label'>Email</label>
                 <input 
                   type="text"
                   placeholder='Enter Employee Email'
@@ -112,8 +155,11 @@ const EmployeeComponent = () => {
                 </input>
                 { errors.email && <div className='invalid-feedback'>{ errors.email } </div> }
               </div>
-              
-              <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+              <div className="text-center">
+              {
+                buttonName()
+              }
+              </div>
             </form>
           </div>
         </div>
